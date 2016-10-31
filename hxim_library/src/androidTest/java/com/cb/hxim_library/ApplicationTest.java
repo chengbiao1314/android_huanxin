53179f683d81a8c82e8803363a120794d4550d91
Ricky Cheng <it_chengbiao@163.com>
2016-10-31 18:30:24 +0800

    commit simple demo..

---
+++ hxim_library/src/androidTest/java/com/cb/hxim_library/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cb.hxim_library;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ hxim_library/src/main/java/com/cb/hxim_library/domain/VideoEntity.java
@@ -0,0 +1,9 @@
+package com.cb.hxim_library.domain;
+
+public class VideoEntity {
+	public int ID;
+	public String title;
+	public String filePath;
+	public int size;
+	public int duration;
+}
